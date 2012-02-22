1824a6c778cee38b42ec321200942239e6d86bde
kacperj <kacperjankowski@gmail.com>
2012-02-22 21:04:02 +0100

    Initial commit

---
+++ NoteShare/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoteShare::Application.config.secret_token = '66b196be0c7eb5db6cc7d207b2c21d01fd023f0e82665cb36f556555e1454c8a4093fc71aae87a541cfc81d4e1357c2a840bbea9001027a666c30f17a82233c0'
